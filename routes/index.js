50c85f124ec726de47b456ad004733671082a68f
antonabramov <anton@juniqe.com>
2017-01-08 18:27:11 +0100

    add new pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: "Сервисный центр Apple - Ремонт iPhone, iPad, MacBook в Service-iPhone",
+    description: "",
+    keywords: ""
+  });
+});
+
+router.get('/remont', function (req, res, next) {
+  res.render('remont', {
+    title: "Ремонт всей техники Apple в Киеве - Сервисный центр Service-iPhone",
+    description: "",
+    keywords: "",
+    menuActive: ''
+  });
+});
+
+router.get('/remont-iphone', function (req, res, next) {
+  res.render('iphones/index', {
+    title: "Ремонт iPhone в Киеве - Сервисный центр Service-iPhone",
+    description: "",
+    keywords: "",
+    menuActive: 'iphone'
+  });
+});
+
+router.get('/remont-ipad', function (req, res, next) {
+  res.render('ipads/index', {
+    title: "Ремонт iPad в Киеве - Сервисный центр Service-iPhone",
+    description: "",
+    keywords: "",
+    menuActive: ''
+  });
+});
+
+router.get('/remont-macbook', function (req, res, next) {
+  res.render('macbooks/index', {
+    title: "Ремонт MacBook в Киеве - Сервисный центр Service-iPhone",
+    description: "",
+    keywords: "",
+    menuActive: ''
+  });
 });
 
 module.exports = router;
